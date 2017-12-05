acdf9d125acfecf79d67716c6e5835bc1bbf0217
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 11:49:57 -0800

    Finished rest of exports

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import Matrix2D from './lib/Matrix2D'
+import Matrix3D from './lib/Matrix3D'
+import ParentTransform from './lib/ParentTransform'
+import SimpleNoise2D from './lib/SimpleNoise2D'
+import Vector2D from './lib/Vector2D'
 
 export {
 	Matrix2D,
+	Vector2D,
+	SimpleNoise2D,
+	ParentTransform,
+	Matrix3D,
 }

61003e38fe54b74c165b0837eb24f87013a3396b
Derek Miranda <derek.miranda.1992@gmail.com>
2019-12-03 16:41:18 -0800

    Refactor SimpleNoise2D to better treeshake
    - convert from class with static methods to one named export fn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import * as SimpleNoise2D from './lib/SimpleNoise2D'
+export { SimpleNoise2D }
+
 export { cssToData } from './lib/matrixUtils'
 export { default as Matrix2D } from './lib/Matrix2D'
 export { default as Matrix3D } from './lib/Matrix3D'
 export { default as ParentTransform } from './lib/ParentTransform'
-export { default as SimpleNoise2D } from './lib/SimpleNoise2D'
 export { default as Vector2D } from './lib/Vector2D'
